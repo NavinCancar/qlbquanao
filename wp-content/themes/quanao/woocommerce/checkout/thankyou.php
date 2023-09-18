8fddaa09ef3cceadfdb771c8364c53887da32520
NavinCancar <nguyenphuonghieu06072002@gmail.com>
2023-09-18 22:24:04 +0700

    Style lỗi nhỏ các giao diện

--- wp-content/themes/quanao/woocommerce/checkout/thankyou.php
+++ wp-content/themes/quanao/woocommerce/checkout/thankyou.php
@@ -17,7 +17,7 @@
 
 defined( 'ABSPATH' ) || exit;
 ?>
-
+<div class="container-fluid">
 <div class="woocommerce-order">
 
 	<?php
@@ -84,5 +84,5 @@ defined( 'ABSPATH' ) || exit;
 		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), null ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 
 	<?php endif; ?>
-
+</div>
 </div>
--- wp-content/themes/quanao/woocommerce/myaccount/my-account.php
+++ wp-content/themes/quanao/woocommerce/myaccount/my-account.php
@@ -35,7 +35,7 @@ defined( 'ABSPATH' ) || exit;
             </a>
             <nav class="collapse show navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0"
                 id="navbar-vertical">
-                <div class="navbar-nav w-100 overflow-hidden" style="height: 410px">
+                <div class="navbar-nav w-100 overflow-hidden">
                     <?php do_action( 'woocommerce_account_navigation' ); ?>
                 </div>
             </nav>
