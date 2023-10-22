47d1c022916b29735646bb5a201d3f64524ca1c5
NavinCancar <nguyenphuonghieu06072002@gmail.com>
2023-10-22 11:10:52 +0700

    Giao diện cho cổng gg

--- wp-content/themes/quanao/woocommerce/myaccount/navigation.php
+++ wp-content/themes/quanao/woocommerce/myaccount/navigation.php
@@ -24,7 +24,6 @@ do_action( 'woocommerce_before_account_navigation' );
 
 
 <?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
-<!-- <li class="<?php// echo wc_get_account_menu_item_classes( $endpoint ); ?>"> -->
 <a class="nav-item nav-link"
     href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>"><?php echo esc_html( $label ); ?>
 </a>
